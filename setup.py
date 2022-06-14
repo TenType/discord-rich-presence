2dbb2442e23a12dbfdae94361c5664a47ea373f7
TenType <55125103+TenType@users.noreply.github.com>
2022-06-13 22:51:06 -0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+import discordrp
+
+setup(
+    name=discordrp.__title__,
+    version=discordrp.__version__,
+    description='A lightweight and safe module for creating custom rich presences on Discord.',
+    url='https://github.com/TenType/discord-rich-presence',
+    author=discordrp.__author__,
+    license=discordrp.__license__,
+    keywords=['discord', 'presence', 'rich presence', 'activity', 'rpc'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: End Users/Desktop',
+        'Operating System :: MacOS',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: Unix',
+        'Typing :: Typed',
+    ],
+    python_requires='>=3.9',
+)
