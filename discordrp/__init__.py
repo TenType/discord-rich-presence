96035bb450d643a3cfe2715d31a18370c708e0c0
TenType <55125103+TenType@users.noreply.github.com>
2023-06-12 11:45:37 -0700

    Bump version

--- discordrp/__init__.py
+++ discordrp/__init__.py
@@ -8,5 +8,5 @@ __title__ = "discord-rich-presence"
 __author__ = "TenType"
 __copyright__ = "Copyright 2022 TenType"
 __license__ = "MIT"
-__version__ = "1.0.2"
+__version__ = "1.0.3"
 __all__ = ("Presence", "PresenceError")
